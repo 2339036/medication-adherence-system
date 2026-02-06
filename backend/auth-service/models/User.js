673bb0be33ade0d232b4f19ad91d83406431c499
Ikhra Nudim <2339036@brunel.ac.uk>
2026-02-06 21:29:11 +0000

    add password reset fields to user model nd add public routes

--- backend/auth-service/models/User.js
+++ backend/auth-service/models/User.js
@@ -18,6 +18,12 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true
+    },
+    resetPasswordToken: {
+      type: String
+    },
+    resetPasswordExpires: {
+      type: Date
     }
   },
   { timestamps: true }
--- backend/auth-service/routes/authRoutes.js
+++ backend/auth-service/routes/authRoutes.js
@@ -7,6 +7,8 @@ const authMiddleware = require("../middleware/authMiddleware");
 // Public routes
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password/:token", authController.resetPassword);
 
 // Protected routes
 router.put("/update-profile", authMiddleware, authController.updateProfile);
